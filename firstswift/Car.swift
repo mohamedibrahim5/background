4822979a55a5979168bf2014bc9407eda4d81107
mohamed ibrahim <mohamedibrahim@MacBook-Pro.local>
2022-06-05 21:08:40 +0200

    adding new class car and struct person

---
+++ firstswift/Car.swift
@@ -0,0 +1,22 @@
+//
+//  Car.swift
+//  firstswift
+//
+//  Created by mohamed ibrahim on 05/06/2022.
+//
+
+import Foundation
+
+class Car
+{
+    var name : String = ""
+    var model = ""
+    var year = 0
+    var person1 = person(name: "ali", number: "001221")
+}
+
+struct person
+{
+    var name : String
+    var number : String
+}
--- firstswift/ViewController.swift
+++ firstswift/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("hello world")
-
+        let a = Car()
+        a.name = "bmw"
+        a.model = "2020"
+        a.year = 2022
+        print(a.name)
+        var person1 = person(name: "ahmed", number: "010202002020")
+        a.person1.name = "mohamed"
+        a.person1.number = "45015"
         // Do any additional setup after loading the view.
     }
 
