05075f709f3155febfc97c5d1906064cd18a8eca
mohamed ibrahim <mohamedibrahim@MacBook-Pro.local>
2022-06-06 00:30:05 +0200

    adding lec 2

--- firstswift/Car.swift
+++ firstswift/Car.swift
@@ -9,10 +9,47 @@ import Foundation
 
 class Car
 {
+    init (name :String)
+    {
+        print("git object")
+    }
     var name : String = ""
     var model = ""
     var year = 0
     var person1 = person(name: "ali", number: "001221")
+    deinit {
+        print("destract the objects")
+    }
+    private var weaks = [1,2,3,4,5,6,7,8,9]
+    subscript (index : Int) -> Int
+    {
+        get
+        {
+            return weaks[index]
+        }
+        set(valuee)
+        {
+            weaks[index] = valuee
+        }
+    }
+  private  var nameee = "mohaed"
+     var namee : String
+    {
+        get
+        {
+            return nameee
+        }
+        set (v)
+        {
+            nameee = v
+        }
+    }
+    var score  = 0 {
+        didSet
+        {
+            print(score)
+        }
+    }
 }
 
 struct person
--- firstswift/ViewController.swift
+++ firstswift/ViewController.swift
@@ -12,15 +12,34 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("hello world")
-        let a = Car()
+        let a = Car(name: "ali")
         a.name = "bmw"
         a.model = "2020"
         a.year = 2022
-        var person1 = person(name: "ahmed", number: "010202002020")
+        _ = person(name: "ahmed", number: "010202002020")
         a.person1.name = "mohamed"
         a.person1.number = "45015"
-        printingdata(car: a)
-        printingdatai(car:a)
+        print(a[0])
+        a[5] = 100
+        print(a[5])
+        
+        
+        print(a.namee)
+        a.namee = "mohanedddd"
+        print(a.namee)
+      //  printingdata(car: a)
+     //   printingdatai(car:a)
+       
+        var name : String
+        {
+            return "ali"
+        }
+  //      print(name)
+   //     print(a.namee)
+    //    print(a.namee)
+        a.score = 100
+        print(a.score)
+   
     }
     func printingdata (car:Car)
     {
